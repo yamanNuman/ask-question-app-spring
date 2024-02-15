f1042196be9d4bf2b9f3c9c0a7d9be743a7e06a7
yamanNuman <yamannuman41@gmail.com>
2024-02-15 13:29:20 +0300

    custom exception

---
+++ src/main/java/exception/CustomExceptionHandler.java
@@ -0,0 +1,21 @@
+package exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(UserNotFoundException.class)
+    public Map<String,String> handleUserNotFoundException(UserNotFoundException exception) {
+        Map<String, String> errorMap = new HashMap<>();
+        errorMap.put("Error Message", exception.getMessage());
+        return errorMap;
+    }
+}
