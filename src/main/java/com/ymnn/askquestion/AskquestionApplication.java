e98e04612aa32962a26086110aba1810da7a507e
yamanNuman <yamannuman41@gmail.com>
2024-02-15 13:19:34 +0300

    project created

---
+++ src/main/java/com/ymnn/askquestion/AskquestionApplication.java
@@ -0,0 +1,13 @@
+package com.ymnn.askquestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskquestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskquestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ymnn/askquestion/AskquestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ymnn.askquestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskquestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
